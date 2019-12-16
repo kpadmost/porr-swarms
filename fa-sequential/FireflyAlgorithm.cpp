c74545462306105b6f818426ce9b7f0541e54aae
Piotr Komorowski <pjpkomorowski@gmail.com>
2019-12-16 22:35:23 +0100

    Parameters.hpp adding as a store for parameters.

--- fa-sequential/FireflyAlgorithm.cpp
+++ fa-sequential/FireflyAlgorithm.cpp
@@ -9,19 +9,17 @@
 #include <stdlib.h>
 #include "FireflyAlgorithm.h"
 
-FireflyAlgorithm::FireflyAlgorithm(int _numberOfFireflies, int _numberOfDimensions, int _dimensionRange, float _attractivityFactor, float _absorptionFactor)
-        : numberOfFireflies(_numberOfFireflies), numberOfDimensions(_numberOfDimensions), dimensionRange(_dimensionRange), attractivityFactor(_attractivityFactor),
-          absorptionFactor(_absorptionFactor){
+FireflyAlgorithm::FireflyAlgorithm(int _numberOfFireflies, int _numberOfDimensions, int _dimensionRange, float _attractivenessFactor, float _absorptionFactor)
+        : parameters(_numberOfFireflies, _numberOfDimensions, _dimensionRange, _attractivenessFactor, _absorptionFactor) {
     this->debugMode;
     InitializeFirefliesTable(this->firefliesTable, _numberOfFireflies, _numberOfDimensions);
     InitializeFirefliesTable(this->firefliesTableTemporary, _numberOfFireflies, _dimensionRange);
     GenerateRandomPositionsOfFireflies();
     CopyToTemporaryFirefliesPositionTable();
-    //PrintCurrentPositionsOfFireflies();
 }
 
 FireflyAlgorithm::~FireflyAlgorithm(){
-    for(int a = 0; a < this->numberOfFireflies; a++){
+    for(int a = 0; a < this->parameters.numberOfFireflies; a++){
         delete this->firefliesTable[a];
         delete this->firefliesTableTemporary[a];
     }
@@ -29,45 +27,44 @@ FireflyAlgorithm::~FireflyAlgorithm(){
     delete this->firefliesTableTemporary;
 }
 
-void FireflyAlgorithm::InitializeFirefliesTable(float** & _firefliesTable, int _numberOfFireflies, int _numberOfDimensions) {
-    _firefliesTable = new float*[_numberOfFireflies];
-    for(int i = 0; i < _numberOfFireflies; i++)
-        _firefliesTable[i] = new float[_numberOfDimensions];
+void FireflyAlgorithm::InitializeFirefliesTable(float** & _firefliesTable, int numberOfFireflies, int numberOfDimensions) {
+    _firefliesTable = new float*[numberOfFireflies];
+    for(int i = 0; i < numberOfFireflies; i++)
+        _firefliesTable[i] = new float[numberOfDimensions];
 };
 
 
 void FireflyAlgorithm::GenerateRandomPositionsOfFireflies() {
-    for(int i = 0; i < this->numberOfFireflies; i++)  // P
-        for(int j = 0; j < this->numberOfDimensions; j++){
-            this->firefliesTable[i][j] = ((float)rand() / RAND_MAX) * 2.0f * this->dimensionRange - 40.0f;
+    for(int i = 0; i < this->parameters.numberOfFireflies; i++)  // P
+        for(int j = 0; j < this->parameters.numberOfDimensions; j++){
+            this->firefliesTable[i][j] = ((float)rand() / RAND_MAX) * 2.0f * this->parameters.dimensionRange - 40.0f;
         }
-
 }
 
 void FireflyAlgorithm::SaveTemporaryFirefliesPosition() {
-    for(int i = 0; i < this->numberOfFireflies; i++)
-        for(int j = 0; j < this->numberOfDimensions; j++){
+    for(int i = 0; i < this->parameters.numberOfFireflies; i++)
+        for(int j = 0; j < this->parameters.numberOfDimensions; j++){
             this->firefliesTable[i][j] = this->firefliesTableTemporary[i][j];
         }
 }
 
 void FireflyAlgorithm::CopyToTemporaryFirefliesPositionTable() {
-    for(int i = 0; i < this->numberOfFireflies; i++)
-        for(int j = 0; j < this->numberOfDimensions; j++){
+    for(int i = 0; i < this->parameters.numberOfFireflies; i++)
+        for(int j = 0; j < this->parameters.numberOfDimensions; j++){
             this->firefliesTableTemporary[i][j] = this->firefliesTable[i][j];
         }
 }
 
 void FireflyAlgorithm::PrintCurrentPositionsOfFireflies() {
-    for(int i = 0; i < this->numberOfFireflies; i++)
-        for(int j = 0; j < this->numberOfDimensions; j++)
+    for(int i = 0; i < this->parameters.numberOfFireflies; i++)
+        for(int j = 0; j < this->parameters.numberOfDimensions; j++)
             std::cout<<this->firefliesTable[i][j]<<std::endl;
 }
 
 float FireflyAlgorithm::CountCostFunction(float* firefly){
     float sigma = 0.0f, pi = 1.0f;
 
-    for(int i = 0; i < this->numberOfDimensions; i++){
+    for(int i = 0; i < this->parameters.numberOfDimensions; i++){
         sigma += pow(firefly[i],2);
         pi *= cos(firefly[i]/(i+1));
 
@@ -83,10 +80,10 @@ void FireflyAlgorithm::RunAlgorithm(int numberOfIterations, bool _debugMode) {
         if(this->debugMode)
             std::cout << "Iteration no. " << iterationNo << std::endl;
 
-        for (int i = 0; i < numberOfFireflies; i++) { // P
-            float *fireflyMoveVector = new float[this->dimensionRange];
+        for (int i = 0; i < parameters.numberOfFireflies; i++) { // P
+            float *fireflyMoveVector = new float[this->parameters.dimensionRange];
             //CleanUpFireflyMoveVector(fireflyMoveVector);
-            for (int j = 0; j < numberOfFireflies; j++) {
+            for (int j = 0; j < parameters.numberOfFireflies; j++) {
 
                 if (CountCostFunction(this->firefliesTable[i]) > CountCostFunction(this->firefliesTable[j]))
                     CalculateFireflyMoveVector(fireflyMoveVector, this->firefliesTable[i], this->firefliesTable[j]);
@@ -103,21 +100,21 @@ void FireflyAlgorithm::RunAlgorithm(int numberOfIterations, bool _debugMode) {
 }
 
 void FireflyAlgorithm::CleanUpFireflyMoveVector(float* & fireflyMoveVector){
-    for(int i = 0; i < this->numberOfDimensions; i++)
+    for(int i = 0; i < this->parameters.numberOfDimensions; i++)
         fireflyMoveVector[i] = 0.0f;
 }
 
 void FireflyAlgorithm::CalculateFireflyMoveVector(float* & fireflyMoveVector, float* mainFirefly, float* secondFirefly){
-    for (int dimension = 0; dimension < this->numberOfDimensions; dimension++) {
+    for (int dimension = 0; dimension < this->parameters.numberOfDimensions; dimension++) {
         float randomValue = ((float)(2*rand() - (RAND_MAX)) /  RAND_MAX);
-        fireflyMoveVector[dimension] += randomValue + this->attractivityFactor *
-                                                      pow(M_E, -(this->absorptionFactor*pow(mainFirefly[dimension]-secondFirefly[dimension],2)));
+        fireflyMoveVector[dimension] += randomValue + this->parameters.attractivenessFactor *
+                                                      pow(M_E, -(this->parameters.absorptionFactor*pow(mainFirefly[dimension]-secondFirefly[dimension],2)));
     }
 }
 
 void FireflyAlgorithm::UpdateFirefliesTemporaryTable(float* fireflyMoveVector, int fireflyNo){
     float previewValue = 0.0f;
-    for (int dimension = 0; dimension < this->numberOfDimensions; dimension++) {
+    for (int dimension = 0; dimension < this->parameters.numberOfDimensions; dimension++) {
         if(this->debugMode)
             std::cout << "Firefly no. " << fireflyNo << " moves in dimension no. " << dimension << " from " <<  firefliesTableTemporary[fireflyNo][dimension] << " to ";
 
